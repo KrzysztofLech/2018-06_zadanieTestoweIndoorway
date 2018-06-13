9277d3036ea6ec65896b3dbac2ac9a2c9148cfe4
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-06-13 22:50:44 +0200

    Data Model added

---
+++ Indoorway/Data Models/PhotoItem.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoItem.swift
+//  Indoorway
+//
+//  Created by Krzysztof Lech on 13.06.2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoItem: Codable {
+    
+    var id:           Int
+    var title:        String
+    var url:          String
+    var thumbnailUrl: String
+}
