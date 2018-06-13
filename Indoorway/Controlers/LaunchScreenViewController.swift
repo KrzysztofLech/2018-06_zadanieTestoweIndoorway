960ebdfb134c0c213efdb9c2475ace33895ae193
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-06-13 16:10:59 +0200

    Initial settings, LaunchScreenVC

---
+++ Indoorway/Controlers/LaunchScreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LaunchScreenViewController.swift
+//  Indoorway
+//
+//  Created by Krzysztof Lech on 13.06.2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    @IBOutlet weak var textContainerView: UIView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 1, animations: {
+            self.textContainerView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        }) { _ in
+            self.performSegue(withIdentifier: "MainVC", sender: nil)
+        }
+    }
+}
---
+++ Indoorway/Controlers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Indoorway
+//
+//  Created by Krzysztof Lech on 13.06.2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
