3f33ae83af1a3ca2e99ab2815f7f9808d0456949
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-06-20 15:31:35 +0200

    Cocoa Pods added: ReachabilitySwift

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.0'
+use_frameworks!
+
+target 'Indoorway' do
+
+   pod 'ReachabilitySwift'
+
+end
---
+++ Pods/Target Support Files/Pods-Indoorway/Pods-Indoorway-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Indoorway : NSObject
+@end
+@implementation PodsDummy_Pods_Indoorway
+@end
