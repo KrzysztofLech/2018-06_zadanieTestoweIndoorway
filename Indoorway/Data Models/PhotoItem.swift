f33992013a4edb30d171c38d810c36b4da078f8c
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-10-16 08:20:27 +0200

    Small fix

--- Indoorway/Data Models/PhotoItem.swift
+++ Indoorway/Data Models/PhotoItem.swift
@@ -10,8 +10,8 @@ import Foundation
 
 struct PhotoItem: Codable {
     
-    var id:           Int
-    var title:        String
-    var url:          String
-    var thumbnailUrl: String
+    let id:           Int
+    let title:        String
+    let url:          String
+    let thumbnailUrl: String
 }
